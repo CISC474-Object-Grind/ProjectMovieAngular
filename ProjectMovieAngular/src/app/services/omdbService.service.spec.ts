26c57b7cd03e4a47d531d9705e1b6b28b3293729
Emily Evans <emilyevans@Emilys-MBP-2.fios-router.home>
2020-05-09 23:20:51 -0400

    trying to figure out service component also renamed to make service calling easier

--- ProjectMovieAngular/src/app/services/omdbService.service.spec.ts
+++ ProjectMovieAngular/src/app/services/omdbService.service.spec.ts
@@ -1,6 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-
-import { OmdbServiceService } from './omdb-service.service';
+import { OmdbServiceService } from './omdbService.service';
 
 describe('OmdbServiceService', () => {
   let service: OmdbServiceService;
