6dac23f1871d8e95516e9f14f3d15fe934dddf04
Emily Evans <emilyevans@Emilys-MacBook-Pro-2.local>
2020-05-12 23:57:23 -0400

    worked on homepage, added in new function for backend API, had to change logo image

--- ExampleApi/out/home/homeController.js
+++ ExampleApi/out/home/homeController.js
@@ -25,6 +25,23 @@ var HomeController = /** @class */ (function () {
             return res.send(result);
         });
     };
+    HomeController.prototype.findShow = function (req, res) {
+        var options = {
+            method: 'GET',
+            url: 'https://imdb8.p.rapidapi.com/title/find',
+            qs: { q: req.params.searchString },
+            headers: {
+                'x-rapidapi-host': 'imdb8.p.rapidapi.com',
+                'x-rapidapi-key': '4cb150e960msh1c6a8e1cccf054ap137887jsn3cbf9acb58bf',
+                useQueryString: true
+            }
+        };
+        request_1.default(options, function (error, response, result) {
+            if (error)
+                return res.status(500).send(error);
+            return res.send(result);
+        });
+    };
     return HomeController;
 }());
 exports.HomeController = HomeController;
