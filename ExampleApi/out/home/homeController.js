0241cf4338e4fcbea400945ff2b6629cec6ec250
Emily Evans <emilyevans@Emilys-MacBook-Pro-2.local>
2020-05-14 14:58:51 -0400

    homepage is finishedgit add .

--- ExampleApi/out/home/homeController.js
+++ ExampleApi/out/home/homeController.js
@@ -15,7 +15,8 @@ var HomeController = /** @class */ (function () {
             qs: { q: req.params.searchString },
             headers: {
                 'x-rapidapi-host': 'imdb8.p.rapidapi.com',
-                'x-rapidapi-key': 'd44c42e1c5msh2c86d382d3ce66fp175fbdjsn1c6180163297',
+                // 'x-rapidapi-key': 'd44c42e1c5msh2c86d382d3ce66fp175fbdjsn1c6180163297',
+                'x-rapidapi-key': 'e1b3a89f74msh0debf0632ebfccfp15b381jsn73bde87118fa',
                 useQueryString: true
             }
         };
@@ -32,7 +33,8 @@ var HomeController = /** @class */ (function () {
             qs: { q: req.params.searchString },
             headers: {
                 'x-rapidapi-host': 'imdb8.p.rapidapi.com',
-                'x-rapidapi-key': 'd44c42e1c5msh2c86d382d3ce66fp175fbdjsn1c6180163297',
+                // 'x-rapidapi-key': 'd44c42e1c5msh2c86d382d3ce66fp175fbdjsn1c6180163297',
+                'x-rapidapi-key': 'e1b3a89f74msh0debf0632ebfccfp15b381jsn73bde87118fa',
                 // 'x-rapidapi-key': '4cb150e960msh1c6a8e1cccf054ap137887jsn3cbf9acb58bf',
                 useQueryString: true
             }
