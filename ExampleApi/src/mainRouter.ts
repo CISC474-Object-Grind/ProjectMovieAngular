fcaffeb3ab4e901e2ea7d19c211df9acd71ba710
Alyssa Burks <burksan@udel.edu>
2020-05-16 22:21:21 -0400

    fixing this

--- ExampleApi/src/mainRouter.ts
+++ ExampleApi/src/mainRouter.ts
@@ -12,16 +12,8 @@ export class MainRouter extends AppRouter{
 
     //adds the child routers to various paths to form the overall API. 
     setupRoutes(): void {
-<<<<<<< HEAD
-        this.addRouter('/security',new SecurityRouter());        
-		this.addRouter('/projects',new ProjectsRouter());
-		this.addRouter('/movies', new MoviesRouter());
-		this.addRouter('/shows', new ShowsRouter());
-		this.addRouter('/omdb', new ProxyRouter());
-=======
         this.addRouter('/security',new SecurityRouter());
 		this.addRouter('/proxy', new ProxyRouter()); //the original API route he gave us
 		this.addRouter('/imdb', new HomeRouter());   //the newer API he provided us
->>>>>>> 92eaa6c98880157b713279215d4d072e10edaf55
     }
 }
